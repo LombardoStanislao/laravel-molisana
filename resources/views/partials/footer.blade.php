ce971d190eaf43eb88c2df2c347b4b8b201fe3d4
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-12-22 20:49:12 +0100

    Finito Footer e Pagina Prodotti

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,193 @@
+<div class="container footer-wrapper">
+
+    <div class="footer-left">
+
+        <img src="{{ asset('images/marchio-sito-test.png') }}" alt="La Molisana | Logo">
+
+        <ul>
+            <li>
+                <a href="#">
+                    Ragione sociale: La Molisana S.P.A.
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Sede legale: Contrada Colle delle Alpi, 100/A
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    86100 - Campobasso (CB)
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Pec: lamolisana@pec.it
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Tel: +39 0874 4981
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Fax: +39 0874 629584
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    info@lamolisana.it (per segnalazioni degli utenti)
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    commerciale@lamolisana.it
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    export@lamolisana.it
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    numero verde 800818081
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    telefono 3801292455
+                </a>
+            </li>
+        </ul>
+
+
+    </div>
+
+    <div class="footer-right">
+
+        <div class="footer-menu">
+            <ul>
+                <li>
+                    <h2> pastificio </h2>
+                </li>
+                <li>
+                    <a href="#">
+                        Il Pastificio
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Grano decorticato a pietra
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Il Molise c'è
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Filiera integrata
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        100 anni di pasta
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Sartoria della pasta
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Spaghetto Quadrato
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Le Persone
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="footer-menu">
+            <ul>
+                <li>
+                    <h2> collezione da chef </h2>
+                </li>
+                <li>
+                    <a href="#">
+                        Collezione da Chef
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Grandi Cucine
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Biologiche
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Quadrate
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="footer-menu last-footer-menu">
+            <ul>
+                <li>
+                    <h2> prodotti </h2>
+                </li>
+                <li>
+                    <a href="#">
+                        Le Classiche
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Le Integrali
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Le Speciali
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Le Biologiche
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Le Gluten-Free
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Le Semole
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Le Extra di Lusso
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+</div>
+
+<img id="footer-background" src="{{ asset('/images/footer-montagne-bottom.jpg')}}" alt="">
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,6 +1,73 @@
 @extends('layout/app')
 
 @section('content')
-    <h1>Questa è la pagina dei prodotti</h1>
+
+    <div class="container">
+
+            <h3>Le Lunghe</h3>
+
+            <div class="card-wrapper">
+
+                @foreach($pasta as $tipologia)
+
+                    @if($tipologia['tipo'] == 'lunga')
+                            <div class="card">
+                                <img src="{{ $tipologia['src']}}" alt="{{ $tipologia['titolo'] }}">
+                                <div class="overlay">
+                                    <a href="#">
+                                        {{ $tipologia['titolo']}}
+                                    </a>
+                                </div>
+                            </div>
+                    @endif
+                @endforeach
+
+            </div>
+
+            <h3>Le corte</h3>
+
+            <div class="card-wrapper">
+
+                @foreach($pasta as $tipologia)
+
+                    @if($tipologia['tipo'] == 'corta')
+                            <div class="card">
+                                <img src="{{ $tipologia['src']}}" alt="{{ $tipologia['titolo'] }}">
+                                <div class="overlay">
+                                    <a href="#">
+                                        {{ $tipologia['titolo']}}
+                                    </a>
+                                </div>
+                            </div>
+                    @endif
+                @endforeach
+
+            </div>
+            <h3>Le cortissime</h3>
+
+            <div class="card-wrapper">
+
+                @foreach($pasta as $tipologia)
+
+                    @if($tipologia['tipo'] == 'cortissima')
+                            <div class="card">
+                                <img src="{{ $tipologia['src']}}" alt="{{ $tipologia['titolo'] }}">
+                                <div class="overlay">
+                                    <a href="#">
+                                        {{ $tipologia['titolo']}}
+                                    </a>
+                                </div>
+                            </div>
+                    @endif
+                @endforeach
+
+            </div>
+
+
+
+
+
+    </div>
+
 
 @endsection
